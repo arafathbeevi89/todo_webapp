cd3edc369b74dfde2d4ae3a8a353cb0784dbdf21
arafathbeevi89 <arafuarafu@gmail.com>
2024-02-13 18:58:02 +0100

    todolist program

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React, { Component } from 'react';
-class App extends Component {
-  render() {
-    return (
-      <p>Hello world!</p>
-    );
-  }
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((todo, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        placeholder="Enter a new task"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 export default App;
